5aa309ea193c401ce9fd04f73d06248169fd2fdf
Anthony Pitluga <tony@pitluga-macbook-2.local>
2009-08-09 17:43:03 -0400

    refactored the instance stack spec to only provision stack once. wierd behavior of before :all

--- spec/integration/instance_spec.rb
+++ spec/integration/instance_spec.rb
@@ -2,7 +2,7 @@ require 'rubygems'
 require 'spec'
 require File.expand_path(File.dirname(__FILE__) + "/../../lib/awsymandias")
 
-describe 'when creating an instance' do
+describe 'a launched instance' do
   
   before :all do
     Awsymandias.access_key_id = ENV['AMAZON_ACCESS_KEY_ID'] 
@@ -20,25 +20,18 @@ describe 'when creating an instance' do
     @stack.terminate!
   end
   
-  describe "the box instance" do
-    it "should be available through a method on stack" do
-      @stack.box.should_not be_nil
-    end
-
-    it "should be running" do
-      @stack.box.running?.should be_true
-    end
+  it "should be available through a method on stack" do
+    @stack.box.should_not be_nil
   end
 
-
-  describe "finding the stack in simple db" do
-    
-    it "should remember the box instance" do
-      found_stack = Awsymandias::EC2::ApplicationStack.find('instances')
-      found_stack.box.should_not be_nil
-      found_stack.box.running?.should be_true
-    end
-    
+  it "should be running" do
+    @stack.box.running?.should be_true
   end
-  
+
+  it "should be saved in simple db" do
+    found_stack = Awsymandias::EC2::ApplicationStack.find('instances')
+    found_stack.box.should_not be_nil
+    found_stack.box.running?.should be_true
+  end
+    
 end
